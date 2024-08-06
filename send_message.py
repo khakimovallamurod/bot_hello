b1644daf0d84f20fcce94f54d47d61264ff91556
khakimovSamdu <allamurodxakimov@gmail.com>
2024-08-06 11:35:21 +0500

    send message

---
+++ send_message.py
@@ -0,0 +1,18 @@
+import requests
+import os
+import time
+TOKEN = os.environ['TOKEN'] 
+URL = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+
+
+def get_from_send_message(chat_id: int, text: str):
+    response = requests.get(URL, params={'chat_id': chat_id, 'text': text})
+        
+    if response.status_code==200:
+        return ('Send message accept')
+    else:
+        return ('Send message error')
+chat_id = '1383186462'
+text = 'Hello World!'
+
+print(get_from_send_message(chat_id, text))
