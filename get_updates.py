fc723fd5057d37824d2a95641fcd4b556857cc77
m1 <uzbzarif@gmail.com>
2024-08-06 11:01:16 +0500

    get last update

--- get_updates.py
+++ get_updates.py
@@ -17,9 +17,22 @@ def get_text_from_update(update):
     return update['message']['text'] 
 
 
-# Get updates
-response = requests.get(URL)
-data = response.json()
 
-# Get results
-result = data['result']
+
+# Loop get updates
+while True:
+    # Get updates
+    response = requests.get(URL)
+    data = response.json()
+
+    # Get results
+    result = data['result']
+
+    # Get last update
+    last_update = result[-1]
+
+    # Get last text from update
+    text = get_text_from_update(last_update)
+
+    # Print text
+    print(text)
