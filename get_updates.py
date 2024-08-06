bd331e1fdac985ac8aa34f6e650d8c1801fbd0c9
khakimovSamdu <allamurodxakimov@gmail.com>
2024-08-06 10:46:52 +0500

    get updetes

--- get_updates.py
+++ get_updates.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import time
 TOKEN = os.environ['TOKEN'] 
 URL = f'https://api.telegram.org/bot{TOKEN}/getUpdates'
 # r = requests.get(f'https://api.telegram.org/bot{TOKEN}/deleteWebhook')
@@ -14,12 +15,20 @@ def get_text_from_update(update):
     Returns:
         str: text from update
     """
+    return update['message']['text']
+
+last_id = -1
+while True:
     response = requests.get(URL)
-    data = response.json()['result']
-    update_id = update['update_id']
-    for update_item in data:
-        if update_item['update_id'] == update_id:
-            return update_item['text']
-    return 'Update not found'
-# Get updates
+    data = response.json()
+    result = data['result']
+    
+    update_last = result[-1]
+    if last_id!=update_last['update_id']:
+        update_end = get_text_from_update(update_last)
+        print(update_end)
+    last_id = update_last['update_id']
+    time.sleep(0.5)
+    
+# Get updates 
 
